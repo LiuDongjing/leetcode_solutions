516ff1b6bdd519277fd1f25fd10da08d5442905b
LiuYangkai <LiuYangkai>
2017-02-17 14:09:11 +0800

    438 sovled

--- 438.cpp
+++ 438.cpp
@@ -1,5 +1,6 @@
 //Find All Anagrams in a String
-class Solution {
+//这个方法会超时
+/*class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         vector<int> index;
@@ -13,5 +14,38 @@ public:
                 index.push_back(i);
         }
         return index;
+    }
+};
+*/
+// 这个方法可以，基本思路是把p变成26个字母的直方图，每次比较也是
+// 直方图比较，移动index的时候修改s的直方图
+class Solution {
+private:
+    bool isEqual(int v1[], int v2[]) {
+        for(int i = 0;i < 26;i++)
+            if(v1[i] != v2[i]) return false;
+        return true;
+    }
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int pvec[26], svec[26];
+        for(int i = 0;i < 26; i++) {
+            pvec[i] = 0;
+            svec[i] = 0;
+        }
+        for(int i = 0;i < p.size();i++) {
+            pvec[p[i] - 'a']++;
+        }
+        vector<int> ret;
+        if(s.size() < p.size())
+            return ret;
+        for(int i = 0; i < p.size();i++) svec[s[i] - 'a']++;
+        for(int i = 0;i <= s.size() - p.size(); i++) {
+            if(isEqual(pvec, svec)) ret.push_back(i);
+            svec[s[i] - 'a']--;
+            if(i + p.size() < s.size())
+                svec[s[i+p.size()] - 'a']++;
+        }
+        return ret;
     }
 };
